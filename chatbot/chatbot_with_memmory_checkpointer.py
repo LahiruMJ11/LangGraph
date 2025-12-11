1e230012178475956db56534e9202487cd88a6f1
LahiruMJ11 <lahiru.jayaweera11@gmail.com>
2025-12-11 12:15:56 +0530

    chat bot implementation

---
+++ chatbot/chatbot_with_memmory_checkpointer.py
@@ -0,0 +1,57 @@
+from typing import TypedDict, Annotated
+from langgraph.graph import add_messages, StateGraph, END
+from langchain_groq import ChatGroq
+from langchain_core.messages import AIMessage, HumanMessage
+from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+
+memory = MemorySaver()
+
+llm = ChatGroq(model = "llama-3.1-8b-instant")
+
+class BasicChatState(TypedDict):
+    messages: Annotated[list, add_messages]
+
+def chatbot(state: BasicChatState):
+    return {
+        "messages": [llm.invoke(state["messages"])]
+    }
+
+graph = StateGraph(BasicChatState)
+
+graph.add_node("chatbot", chatbot)
+graph.set_entry_point("chatbot")
+graph.add_edge("chatbot", END)
+
+app = graph.compile(checkpointer = memory)
+
+config = {"configurable": {
+    "thread_id": 1
+}}
+
+response1 = app.invoke({
+    "messages": HumanMessage(content = "Hi I'm Lahiru")
+}, config = config)
+
+response2 = app.invoke({
+    "messages": HumanMessage(content = "What's my name?")
+}, config = config)
+
+#print(app.get_state(config = config))
+
+# print(response1)
+# print("\n")
+# print(response2)
+
+while True:
+    user_input = input("User: ")
+    if user_input == "exit":
+        break
+    else:
+        response = app.invoke({
+            "messages": HumanMessage(content=user_input)
+        }, config = config)
+
+        print("AI: " + response["messages"][-1].content)
